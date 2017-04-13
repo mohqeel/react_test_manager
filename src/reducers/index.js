bc4eb2f87e1a40b6821e0b2c7ae5f80ba972cb58
mohagagg <moha.gagg@gmail.com>
2017-04-13 12:16:22 -0700

    added scene transistion from emp create to emp list

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import AuthReducer from './AuthReducer';
-import EmployeeFormReducer from './EmployeeFormReducer';
-
-export default combineReducers({
-  auth: AuthReducer,
-  employeeForm: EmployeeFormReducer
-});
